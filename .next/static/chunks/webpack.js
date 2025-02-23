beadb493f25ba1ea034a5a2a9a3d1a179b6369a1
Calel33 <62570052+Calel33@users.noreply.github.com>
2025-02-22 21:29:25 -0500

    exbuild1

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -89,18 +89,6 @@
 /******/ 		};
 /******/ 	})();
 /******/ 	
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => (module['default']) :
-/******/ 				() => (module);
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/create fake namespace object */
 /******/ 	(() => {
 /******/ 		var getProto = Object.getPrototypeOf ? (obj) => (Object.getPrototypeOf(obj)) : (obj) => (obj.__proto__);
@@ -190,7 +178,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("ac91ec38eb30b7c3")
+/******/ 		__webpack_require__.h = () => ("174e49d3dc47b572")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
